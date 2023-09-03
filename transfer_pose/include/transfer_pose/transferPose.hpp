35fa15b3331a3a7c16192536dcbe2b5765d2934c
huihui500 <1761374236@qq.com>
2023-09-03 23:04:55 +0800

    [fix] Eigen is ok

--- transfer_pose/include/transfer_pose/transferPose.hpp
+++ transfer_pose/include/transfer_pose/transferPose.hpp
@@ -16,8 +16,10 @@
 #include <tf2/convert.h>
 #include <tf2/transform_datatypes.h>
 #include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <tf2_ros/transform_listener.h>
 
 #include <Eigen/Core>
+#include <Eigen/Geometry>
 class transferPose
 {
 private:
@@ -26,9 +28,8 @@ private:
     ros::NodeHandle nh;
 
     // transform "world" 2 "pose1"
-    // 创建一个TransformStamped消息来表示坐标变换
-    // geometry_msgs::TransformStamped transform_world2pose_;
-    geometry_msgs::TransformStamped transformStamped;
+    Eigen::Affine3d inverse_transform;
+
     // 现在pose2_in_pose1_frame包含了pose2在pose1坐标系下的位姿
     void imageCallback(const sensor_msgs::Image::ConstPtr &msg);
 
@@ -87,21 +88,20 @@ bool transferPose::parseCsv(std::string path)
         }
 
         geometry_msgs::PoseStamped pose_stamped;
-        
+
         uint64_t time_int = atof(words[7].c_str());
-        
+
         ros::Time timestamp(time_int / 1000000000, (time_int % 1000000000));
         pose_stamped.header.frame_id = "global";
         pose_stamped.header.stamp = timestamp;
         pose_stamped.pose.position.x = atof(words[3].c_str());
         pose_stamped.pose.position.y = atof(words[4].c_str());
         pose_stamped.pose.position.z = atof(words[5].c_str());
-        
+
         tf2::Quaternion quaternion;
         quaternion.setRPY(atof(words[14].c_str()), atof(words[13].c_str()), atof(words[12].c_str()));
         pose_stamped.pose.orientation = tf2::toMsg(quaternion);
         v_path_pose.push_back(pose_stamped);
-        
     }
     csv_data.close();
     ROS_INFO("--Parse success; over");
@@ -116,23 +116,54 @@ void transferPose::imageCallback(const sensor_msgs::Image::ConstPtr &msg)
     {
         init_flag_ = true;
         ROS_INFO("Initialize success!");
-        // 设置transform
-        transformStamped.header.stamp = ros::Time::now(); // 设置时间戳
-        transformStamped.header.frame_id = "world";       // 设置世界坐标系
-        transformStamped.child_frame_id = "pose1";        // 设置pose1为子坐标系
 
-        // 将pose1的位姿信息转换为坐标变换矩阵
-        transformStamped.transform.translation.x = v_path_pose[0].pose.position.x;
-        transformStamped.transform.translation.y = v_path_pose[0].pose.position.y;
-        transformStamped.transform.translation.z = v_path_pose[0].pose.position.z;
-        transformStamped.transform.rotation = v_path_pose[0].pose.orientation;
+        Eigen::Affine3d forward_transform;
+        forward_transform.translation() << v_path_pose[0].pose.position.x,
+            v_path_pose[0].pose.position.y,
+            v_path_pose[0].pose.position.z;
+        Eigen::Quaterniond quat = Eigen::Quaterniond(v_path_pose[0].pose.orientation.w,
+                                                     v_path_pose[0].pose.orientation.x,
+                                                     v_path_pose[0].pose.orientation.y,
+                                                     v_path_pose[0].pose.orientation.z).normalized();
+        forward_transform.linear() = Eigen::AngleAxisd(quat).toRotationMatrix();
+
+        inverse_transform = forward_transform.inverse();
     }
 
     // if (init_flag_ && v_path_pose[msg->header.seq].header.stamp == timestamp_)
     if (init_flag_)
     {
         geometry_msgs::PoseStamped pose2_in_pose1_frame;
-        tf2::doTransform(v_path_pose[msg->header.seq], pose2_in_pose1_frame, transformStamped);
+
+        // 执行变换
+        Eigen::Vector3d original_position(v_path_pose[msg->header.seq].pose.position.x,
+                                          v_path_pose[msg->header.seq].pose.position.y,
+                                          v_path_pose[msg->header.seq].pose.position.z);
+
+        Eigen::Quaterniond original_quaternion(v_path_pose[msg->header.seq].pose.orientation.w,
+                                               v_path_pose[msg->header.seq].pose.orientation.x,
+                                               v_path_pose[msg->header.seq].pose.orientation.y,
+                                               v_path_pose[msg->header.seq].pose.orientation.z);
+
+        std::cout << "original position:\n" << original_position << std::endl
+                  << "original_quat:\n" << original_quaternion.toRotationMatrix() << std::endl;
+
+        Eigen::Quaterniond transformed_quaternion = Eigen::Quaterniond(inverse_transform.rotation()) * original_quaternion;
+        Eigen::Vector3d transformed_position = inverse_transform * original_position;
+
+
+        std::cout << "transformed position:\n" << transformed_position << std::endl
+                  << "transformed_quat:\n" << transformed_quaternion.toRotationMatrix() << std::endl;
+        // 更新geometry_msgs::Pose对象的位置
+        pose2_in_pose1_frame.pose.position.x = transformed_position.x();
+        pose2_in_pose1_frame.pose.position.y = transformed_position.y();
+        pose2_in_pose1_frame.pose.position.z = transformed_position.z();
+
+        // 更新geometry_msgs::Pose对象的方向
+        pose2_in_pose1_frame.pose.orientation.x = transformed_quaternion.x();
+        pose2_in_pose1_frame.pose.orientation.y = transformed_quaternion.y();
+        pose2_in_pose1_frame.pose.orientation.z = transformed_quaternion.z();
+        pose2_in_pose1_frame.pose.orientation.w = transformed_quaternion.w();
 
         const double kDegreeToRadian = M_PI / 180.;
         // for EuROC dataset
@@ -142,9 +173,9 @@ void transferPose::imageCallback(const sensor_msgs::Image::ConstPtr &msg)
 
         Eigen::Matrix<double, 6, 6> cov;
         cov.setZero();
-        cov.block<3,3>(0,0) = Eigen::Matrix3d::Identity() * sigma_pv * sigma_pv;
-        cov.block<2,2>(3,3) = Eigen::Matrix2d::Identity() * sigma_rp * sigma_rp;
-        cov(5,5) = sigma_yaw * sigma_yaw;
+        cov.block<3, 3>(0, 0) = Eigen::Matrix3d::Identity() * sigma_pv * sigma_pv;
+        cov.block<2, 2>(3, 3) = Eigen::Matrix2d::Identity() * sigma_rp * sigma_rp;
+        cov(5, 5) = sigma_yaw * sigma_yaw;
 
         geometry_msgs::PoseWithCovarianceStamped pose_cov_stamped;
         pose_cov_stamped.header = v_path_pose[msg->header.seq].header;
